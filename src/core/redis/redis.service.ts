f6dbff1961f4f24f6f0f8da08e6d5f66db47369b
Omadbek <omadbek@example.com>
2025-06-28 14:13:34 +0500

    env yuklandika

--- src/core/redis/redis.service.ts
+++ src/core/redis/redis.service.ts
@@ -3,18 +3,32 @@ import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService implements OnModuleInit {
-    private redis_client: Redis
+    private redis_client: Redis;
+
     async onModuleInit() {
-        this.redis_client = new Redis()
-    }
-    async set(key: string, code: string, second: number) {
-        return await this.redis_client.set(key, code, 'EX', second)
-    }
-    async get(key: string) {
-       return  await this.redis_client.get(key)
-    }
-    async del(key: string) {
-        return await this.redis_client.del(key)
+        this.redis_client = new Redis({
+            host: '127.0.0.1',
+            port: 6379
+        });
+
+        this.redis_client.on('connect', () => {
+            console.log('Redis connected (localhost)');
+        });
+
+        this.redis_client.on('error', (err) => {
+            console.error(' Redis error:', err);
+        });
     }
 
+    async set(key: string, code: string, second: number) {
+        return await this.redis_client.set(key, code, 'EX', second);
+    }
+
+    async get(key: string) {
+        return await this.redis_client.get(key);
+    }
+
+    async del(key: string) {
+        return await this.redis_client.del(key);
+    }
 }
